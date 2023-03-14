d376ae8dfc2ac020c4b2f0513409ba9f43117c96
VitaliyBaranovskyy <40470956+VitaliyBaranovskyy@users.noreply.github.com>
2023-03-14 13:20:48 +0200

    Update PrimeNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        
+        int i =0;
+        while (i<=printToInclusive){
+        if (i%2 !=0)
+        System.out.println(i);
+        i++;
+        
+        } 
+    }
+    
+    public static void main (String[] args){
+       PrimeNumbers main = new PrimeNumbers();
+       main.printPrimeNumbers(10);
+        
     }
 }
+   
