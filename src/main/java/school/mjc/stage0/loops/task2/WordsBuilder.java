f6733d89f931971927fc36e1e1ac706fecf48aad
VitaliyBaranovskyy <40470956+VitaliyBaranovskyy@users.noreply.github.com>
2023-03-14 14:09:50 +0200

    Update WordsBuilder.java

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -1,7 +1,15 @@
 package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
-    public void buildPhrase(char... chars) {
+    public void buildPhrase(char ch1, char ch2, char ch3, char ch4, char ch5, char ch6, char ch7, char ch8, char ch9, char ch10) {
+        
+        System.out.println(ch1+ "" + ch2+""+ch3+""+ch4+""+ch5+" "+ch6+""+ch7+""+ch8+""+ch9+""+ch10);
 
+    }
+    
+    public static void main (String [] args){
+        WordsBuilder main = new WordsBuilder();
+        main.buildPhrase('H','e','l','l','o','l','o','o','p','s');
+        
     }
 }
