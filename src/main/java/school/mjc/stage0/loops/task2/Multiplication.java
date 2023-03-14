5fa7938ce8eb44f370419c62a7d7f248f4cff350
VitaliyBaranovskyy <40470956+VitaliyBaranovskyy@users.noreply.github.com>
2023-03-14 14:23:55 +0200

    Update Multiplication.java

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+       int i = 2;
+        int b = 1;
+        while (b <= multiplyByAndToInclusive){
+            int c = i*b;
+            System.out.println(c);
+            b++;
+        }  
+    }
+    
+    public static void main (String[] args){
+       Multiplication main = new Multiplication();
+        main.printMultiplied(5);
+        
     }
 }
