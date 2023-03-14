4b34c081e3a5a13bd93e64e6426e5308274125f0
VitaliyBaranovskyy <40470956+VitaliyBaranovskyy@users.noreply.github.com>
2023-03-14 12:51:00 +0200

    Update PowerOfTwo.java

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,20 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
+        int i = 2;
+        int a = 0;
+        int c = 1;
+        
+        while ( a<=power){
+         c = c*i;
+            System.out.println(i + "в ступені " + a + " = " + c);
+            a++;
+        }
 
+    }
+    
+    public static void main (String [] args){
+       PowerOfTwo main =new PowerOfTwo();
+        main.printPower(8);
     }
 }
